46aa227bd6e2bfca972e283965708bf9cb0b40a8
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2021-01-12 21:47:36 +0000

    top bar added

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import { StatusBar } from "expo-status-bar"
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, SafeAreaView } from "react-native"
 import Header from "./components/header"
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <SafeAreaView />
       <Header />
-      <Text> Open up App.js to start working on your app! </Text>
       <StatusBar style="auto" />
+      <Text> Open up App.js to start working on your app! </Text>
     </View>
   )
 }
--- components/header.jsx
+++ components/header.jsx
@@ -1,29 +1,50 @@
 import { StatusBar } from "expo-status-bar"
 import React from "react"
 import Icon from "react-native-vector-icons/Ionicons"
-import { StyleSheet, Text, View, TextInput, drop } from "react-native"
+import { StyleSheet, Text, View, TextInput } from "react-native"
+// import BottomSheet from "reanimated-bottom-sheet"
 
 const Header = () => {
   return (
     <View style={style.header}>
-      <Icon name="ios-search" size={20} color="#000" />
-      <TextInput style={style.input}>
-        <Icon name="ios-location" size={25} color="#000" />
-      </TextInput>
+      <Icon
+        name="ios-location"
+        style={{ alignContent: "center" }}
+        size={25}
+        color="#000"
+      />
+      <View style={style.now}>
+        <Icon name="ios-location" size={20} color="#000" />
+        <Text style={{ paddingLeft: 5 }}>Now</Text>
+        <Icon name="ios-arrow-left" size={20} color="#000" />
+      </View>
     </View>
   )
 }
 
+const spacing = {
+  padding: 10,
+  margin: 10,
+  borderRadius: 100,
+}
+
 const style = StyleSheet.create({
-  input: {
-    backgroundColor: "lightgrey",
-    padding: 10,
-    borderRadius: 100,
-  },
   header: {
-    flex: 1,
-    padding: 20,
-    margin: 20,
+    backgroundColor: "lightgrey",
+    flexDirection: "row",
+    padding: spacing.padding,
+    margin: spacing.margin,
+    borderRadius: spacing.borderRadius,
+    justifyContent: "space-between",
+    alignContent: "center",
+  },
+  now: {
+    backgroundColor: "white",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 5,
+    borderRadius: spacing.borderRadius,
+    alignContent: "center",
   },
 })
 
