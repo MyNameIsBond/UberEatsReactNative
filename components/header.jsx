776d45b1ea9548f765ecb63b6b989e5a0b87d5e4
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2021-01-12 11:24:09 +0000

    tefield done!

--- components/header.jsx
+++ components/header.jsx
@@ -1,22 +1,29 @@
 import { StatusBar } from "expo-status-bar"
 import React from "react"
-import { StyleSheet, Text, View, TextInput } from "react-native"
+import Icon from "react-native-vector-icons/Ionicons"
+import { StyleSheet, Text, View, TextInput, drop } from "react-native"
 
 const Header = () => {
   return (
     <View style={style.header}>
-      <TextInput style={style.input}>D</TextInput>
+      <Icon name="ios-search" size={20} color="#000" />
+      <TextInput style={style.input}>
+        <Icon name="ios-location" size={25} color="#000" />
+      </TextInput>
     </View>
   )
 }
 
 const style = StyleSheet.create({
   input: {
-    backgroundColor: "aliceblue",
+    backgroundColor: "lightgrey",
+    padding: 10,
+    borderRadius: 100,
   },
   header: {
-    backgroundColor: "white",
-    padding: 10,
+    flex: 1,
+    padding: 20,
+    margin: 20,
   },
 })
 
